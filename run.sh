8a18a71e588fd6b5faa702982c395f85fa0021a6
caosiyang <caosiyang@qiyi.com>
2015-01-21 21:16:06 +0800

    网络中断自动重连，支持oplog断点续写，DuplicateKeyError容错处理，writeConcern设置为1，确认写操作成功，保证数据一致性

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-python main.py --from nl-mongodb-online003-bjdxt.qiyi.virtual:27018 --to 10.15.188.223:27017 --db qiyirc --coll qiyirc --query "{'uid': {'\$regex': '^tv_'}}"
+#python main.py --from nl-mongodb-online003-bjdxt.qiyi.virtual:27018 --to 10.15.188.223:27017 --db qiyirc --coll qiyirc --query "{'uid': {'\$regex': '^tv_'}}"
+python main.py --from demo-mongodb-dev001-jylt.qiyi.virtual:27018 --to 10.15.188.223:27017 --db qiyirc --coll qiyirc --query "{'uid': {'\$regex': '^tv_'}}"
