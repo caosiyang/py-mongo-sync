14c04ca0e65363e4883cf743e221eb20083e1f17
caosiyang <caosiyang@qiyi.com>
2018-09-04 15:44:54 +0800

    code tuning

--- mongosync/multi_oplog_replayer.py
+++ mongosync/multi_oplog_replayer.py
@@ -8,49 +8,43 @@ from mongosync.logger import Logger
 log = Logger.get()
 
 
-class OplogVector:
+class OplogVector(object):
     """ A set of oplogs with same namespace.
     """
-    def __init__(self):
-        self._dbname = ''
-        self._collname = ''
+    def __init__(self, dbname, collname):
+        self._dbname = dbname
+        self._collname = collname
         self._oplogs = []
 
-class MultiOplogReplayer:
-    """ Concurent oplog replaying for MongoDB.
+
+class MultiOplogReplayer(object):
+    """ Concurrent oplog replayer for MongoDB.
     """
-    def __init__(self, mc, n_writers):
+    def __init__(self, mc, n_writers=10, batch_size=40):
+        """
+        Parameter:
+          - n_writers: maximum coroutine count
+          - batch_size: maximum oplog count in a batch, 40 is empiric value
+        """
         assert isinstance(mc, Mongo)
         assert n_writers > 0
+        assert batch_size > 0
         self._mc = mc
         self._pool = gevent.pool.Pool(n_writers)
+        self._batch_size = batch_size
         self._map = {}
         self._count = 0
         self._last_optime = None
 
     def clear(self):
+        """ Clear oplogs.
+        """
         self._map.clear()
         self._count = 0
 
-    #def push(self, oplog):
-    #    op = oplog['op']
-    #    ns = oplog['ns']
-    #    if ns not in self._map:
-    #        self._map[ns] = []
-    #    if op == 'u':
-    #        self._map[ns].append(pymongo.UpdateOne({'_id': oplog['o2']['_id']}, oplog['o']))
-    #    elif op == 'i':
-    #        self._map[ns].append(pymongo.ReplaceOne({'_id': oplog['o']['_id']}, oplog['o'], upsert=True))
-    #    elif op == 'd':
-    #        self._map[ns].append(pymongo.DeleteOne({'_id': oplog['o']['_id']}))
-    #    else:
-    #        log.error('invaid op: %s' % oplog)
-    #        assert op in ['i', 'u', 'd']
-    #    self._count += 1
-    #    self._last_optime = oplog['ts']
-
     def push(self, oplog):
-        op = oplog['op']
+        """ Push oplog and group by namespace.
+        """
         ns = oplog['ns']
         if ns not in self._map:
             self._map[ns] = []
@@ -58,69 +52,29 @@ class MultiOplogReplayer:
         self._count += 1
         self._last_optime = oplog['ts']
 
-    #def apply(self):
-    #    for ns, reqs in self._map.iteritems():
-    #        dbname, collname = mongo_utils.parse_namespace(ns)
-    #        self._pool.spawn(self._mc.bulk_write, dbname, collname, reqs)
-    #    self._pool.join()
-
     def apply(self):
+        """ Apply oplogs.
+        """
         oplog_vecs = []
         for ns, oplogs in self._map.iteritems():
             dbname, collname = mongo_utils.parse_namespace(ns)
-            n = len(oplogs) / 40 + 1  # 40 is empiric value
+            n = len(oplogs) / self._batch_size + 1
             if n == 1:
-                vec = OplogVector()
-                vec._dbname = dbname
-                vec._collname = collname
+                vec = OplogVector(dbname, collname)
                 for oplog in oplogs:
-                    op = oplog['op']
-                    if op == 'u':
-                        is_replace = True
-                        for key in oplog['o'].iterkeys():
-                            if key and key[0] == '$':
-                                is_replace = False
-                        if not is_replace:
-                            vec._oplogs.append(pymongo.UpdateOne({'_id': oplog['o2']['_id']}, oplog['o']))
-                        else:
-                            vec._oplogs.append(pymongo.ReplaceOne({'_id': oplog['o2']['_id']}, oplog['o'], upsert=True))
-                    elif op == 'i':
-                        vec._oplogs.append(pymongo.ReplaceOne({'_id': oplog['o']['_id']}, oplog['o'], upsert=True))
-                    elif op == 'd':
-                        vec._oplogs.append(pymongo.DeleteOne({'_id': oplog['o']['_id']}))
-                    else:
-                        log.error('invaid op: %s' % oplog)
-                        assert op in ['i', 'u', 'd']
+                    op = self.__convert(oplog)
+                    assert op is not None
+                    vec._oplogs.append(op)
                 oplog_vecs.append(vec)
             else:
-                vecs = []
-                for i in range(0, n):
-                    vec = OplogVector()
-                    vec._dbname = dbname
-                    vec._collname = collname
-                    vecs.append(vec)
+                vecs = [OplogVector(dbname, collname) for i in xrange(n)]
                 for oplog in oplogs:
-                    op = oplog['op']
-                    if op == 'u':
-                        m = self.hash(oplog['o2']['_id'])
-                        is_replace = True
-                        for key in oplog['o'].iterkeys():
-                            if key and key[0] == '$':
-                                is_replace = False
-                                break
-                        if not is_replace:
-                            vecs[m % n]._oplogs.append(pymongo.UpdateOne({'_id': oplog['o2']['_id']}, oplog['o']))
-                        else:
-                            vecs[m % n]._oplogs.append(pymongo.ReplaceOne({'_id': oplog['o2']['_id']}, oplog['o'], upsert=True))
-                    elif op == 'i':
-                        m = self.hash(oplog['o']['_id'])
-                        vecs[m % n]._oplogs.append(pymongo.ReplaceOne({'_id': oplog['o']['_id']}, oplog['o'], upsert=True))
-                    elif op == 'd':
-                        m = self.hash(oplog['o']['_id'])
-                        vecs[m % n]._oplogs.append(pymongo.DeleteOne({'_id': oplog['o']['_id']}))
-                    else:
-                        log.error('invaid op: %s' % oplog)
-                        assert op in ['i', 'u', 'd']
+                    op = self.__convert(oplog)
+                    assert op is not None
+                    # filter of UpdateOne/ReplaceOne/DeleteOne is {'_id': ObjectID}
+                    # @ref https://github.com/mongodb/mongo-python-driver/blob/master/pymongo/operations.py
+                    m = self.__hash(op._filter['_id'])
+                    vecs[m % n]._oplogs.append(op)
                 oplog_vecs.extend(vecs)
 
         for vec in oplog_vecs:
@@ -128,18 +82,46 @@ class MultiOplogReplayer:
                 self._pool.spawn(self._mc.bulk_write, vec._dbname, vec._collname, vec._oplogs)
         self._pool.join()
 
-    def hash(self, oid):
-        """ Hash ObjectID.
+    def count(self):
+        """ Return count of oplogs.
+        """
+        return self._count
+
+    def last_optime(self):
+        """ Return timestamp of the last oplog.
+        """
+        return self._last_optime
+
+    def __convert(self, oplog):
+        """ Convert oplog to operation that supports bulk write.
+        """
+        op = oplog['op']
+        if op == 'u':
+            # it could be an update or replace
+            # @ref https://docs.mongodb.com/manual/reference/limits/#naming-restrictions
+            is_update = False
+            for key in oplog['o'].iterkeys():
+                if key[0] == '$':
+                    is_update = True
+                    break
+            if is_update:
+                return pymongo.operations.UpdateOne({'_id': oplog['o2']['_id']}, oplog['o'])
+            else:
+                return pymongo.operations.ReplaceOne({'_id': oplog['o2']['_id']}, oplog['o'], upsert=True)
+        elif op == 'i':
+            return pymongo.operations.ReplaceOne({'_id': oplog['o']['_id']}, oplog['o'], upsert=True)
+        elif op == 'd':
+            return pymongo.operations.DeleteOne({'_id': oplog['o']['_id']})
+        else:
+            log.error('invaid op: %s' % oplog)
+            return None
+
+    def __hash(self, oid):
+        """ Hash ObjectID with murmurhash3.
         """
         try:
             # str(oid) may contain non-ascii characters
             m = mmh3.hash(str(oid), signed=False)
-        except:
+        except Exception as e:
             m = 0
         return m
-
-    def count(self):
-        return self._count
-
-    def last_optime(self):
-        return self._last_optime
